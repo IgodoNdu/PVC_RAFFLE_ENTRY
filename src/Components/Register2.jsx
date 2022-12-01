c558bc3badd0d5a060439c80555f769a65f7ca9a
IgodoNdu <darkbluetechmail@gmail.com>
2022-12-01 09:29:07 +0100

    PVC Raffle App
    PVC Raffle Entry App with MERN

---
+++ src/Components/Register2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Register2() {
+  return (
+    <div id='contact'>
+      <h1>Register For The Draw</h1>
+      <form>
+        <input type="text" placeholder="fullname" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Phone" />
+        <input type="submit" value="REGISTER" />
+      </form>
+    </div>
+  )
+}
+
+export default Register2
